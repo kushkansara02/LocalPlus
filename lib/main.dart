1ac647ef09938bf1bb454fb45c3d59b7959e62ee
kushkansara02 <kushkansara02@gmail.com>
2020-08-21 22:41:02 -0400

    Added basic text on app home screen

--- lib/main.dart
+++ lib/main.dart
@@ -22,67 +22,88 @@ class MyApp extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Welcome, Ad00t!',
-                  style: TextStyle(
-                      fontFamily: 'Montserrat', fontWeight: FontWeight.bold),
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      'Welcome, Ad00t!',
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
                 ),
               ),
             ),
             SizedBox(
               height: 20,
             ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Your current location is: ',
-                  style: TextStyle(
-                      fontFamily: 'Montserrat', fontWeight: FontWeight.bold),
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Your current location is:',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat', fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
             ),
             SizedBox(
               height: 20,
             ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'You currently have X items selected in Y shopping lists',
-                  style: TextStyle(
-                      fontFamily: 'Montserrat', fontWeight: FontWeight.bold),
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'You currently have X items selected in Y shopping lists',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat', fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
             ),
             SizedBox(
               height: 20,
             ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Your most recent list would take you to N stores within an M km radius',
-                  style: TextStyle(
-                      fontFamily: 'Montserrat', fontWeight: FontWeight.bold),
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Your most recent list would take you to N stores within an M km radius',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat', fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
             ),
             SizedBox(
               height: 20,
             ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Info (Expand)',
-                  style: TextStyle(
-                      fontFamily: 'Montserrat', fontWeight: FontWeight.bold),
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Info (Expand)',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat', fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
+            ),
+            SizedBox(
+              height: 50,
             ),
           ],
         ),
